7ccda1c786a65854e6e495bcaa53bcfa6f959c58
JustKitty <45317566+cutekittieee@users.noreply.github.com>
2020-09-15 09:56:03 +0200

    Create int.asm

---
+++ chapter5/ex01/int.asm
@@ -0,0 +1,21 @@
+segment .data
+a dq 175
+b dq 0x1001
+c dq -295
+d dq -1
+
+segment .text
+global _start
+_start:
+mov rax, [a]
+mov rbx, [b]
+mov rcx, [c]
+mov rdx, [d]
+
+add rax, rbx
+add rax, rcx
+add rax, rdx
+
+mov eax, 1
+mov ebx, 0
+int 0x80
